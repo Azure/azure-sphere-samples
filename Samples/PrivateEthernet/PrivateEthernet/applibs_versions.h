8e6ed33cbd139dda6f2c71442e00ce8ae5abe976
Mark Cawston <macawsto@microsoft.com>
2018-11-28 16:17:49 +0000

    Initial release of Private Ethernet sample.

---
+++ Samples/PrivateEthernet/PrivateEthernet/applibs_versions.h
@@ -0,0 +1,7 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+#pragma once
+// This header defines which struct versions will be used for applibs APIs.
+#define WIFICONFIG_STRUCTS_VERSION 1
+#define UART_STRUCTS_VERSION 1
